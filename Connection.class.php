7bb3152d9671c2546ba239dc1d79391a0072617e
Lucas Renan <lucas@freelancersbrasil.com>
2010-08-11 18:29:06 -0300

    add query functionality

--- Connection.class.php
+++ Connection.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Description of connectionclass
+ * Description of Connection
  *
  * @author Lucas
  */
@@ -20,6 +20,13 @@ class Connection extends mysqli {
     private static $_instance = null;
 
     /**
+     * 
+     */
+    public function  __destruct() {
+        $this->close();
+    }
+
+    /**
      *
      * @return Connection
      */
@@ -33,10 +40,47 @@ class Connection extends mysqli {
     /**
      *
      */
+    public function connect() {
+        if(!self::$_connected) {
+            parent::__construct(
+                    "localhost",
+                    "user",
+                    "pass",
+                    "database"
+            );
+
+            if(mysqli_connect_errno()) {
+                throw new Exception("Connection failed: ".mysqli_connect_error());
+            }
+
+            self::$_connected = true;
+        }
+    }
+
+    /**
+     *
+     */
     public function close() {
         if(self::$_connected) {
             parent::close();
             self::$_connected = false;
+        }
+    }
+
+    /**
+     *
+     * @param string $sql
+     * @return mysqli_result
+     */
+    public function query($sql) {
+        $this->connect();
+        $result = parent::query($sql);
+
+        if($result) {
+            return $result;
+        }
+        else {
+            throw new Exception('Query Exception: '.mysqli_error($this).' num:'.mysqli_errno($this));
         }
     }
 
